146ff336d1127dd7e6c036a8b1dd8162267eb65c
Michael VanderHelm <mikevh@gmail.com>
2016-03-24 15:01:41 -0700

    foo update

--- TodoJob/Program.cs
+++ TodoJob/Program.cs
@@ -17,8 +17,8 @@ namespace TodoJob
         // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main(string[] args)
         {
-            var env_arg = Environment.GetEnvironmentVariable("WEBJOBS_COMMAND_ARGUMENTS");
-            Console.WriteLine(env_arg);
+            Console.WriteLine("FOOOOO");
+            
 
             Console.WriteLine("argumnets: " + string.Join(", ", args));
             var cmd = !args.Any() ? "test" : args[0];
